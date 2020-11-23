b35cf00fb4d9da4ff9b1d6602b6470247a02fe1f
fworeiaixuexi <74755055+fworeiaixuexi@users.noreply.github.com>
2020-11-23 18:39:47 +0800

    112301.App.java
    输入1产生两个随机数R(x,y)

---
+++ 112301.App.java
@@ -0,0 +1,15 @@
+package pac;
+
+import java.util.Scanner;
+
+public class App {
+public static void main(String[] args) {
+Scanner scanner=new Scanner(System.in);
+while (true) {
+int user_num=scanner.nextInt();
+if (user_num==1) {
+System.out.println("R("+(int)(Math.random()*100)+","+(int)(Math.random()*100)+")");
+}
+}
+}
+}
