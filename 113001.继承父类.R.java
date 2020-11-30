63746e63fa0eef2455c6fe5653e31c2f75db84f2
fworeiaixuexi <74755055+fworeiaixuexi@users.noreply.github.com>
2020-11-30 15:37:03 +0800

    113001.继承父类.R.java

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -1,5 +1,8 @@
 public class R {
-	static String boy_filePath="img/boy.gif";
-	static String girl_filePath="ima/girl.gif";
-	static int [][]array2d={{1,2,3},{4,5,6}};
+	 static String boy_filePath="img/boy.gif";
+	 static String girl_filePath="ima/girl.gif";
+	 static int [][]array2d={
+			 {1,2,3},
+			 {4,5,6}
+			 };
 }
