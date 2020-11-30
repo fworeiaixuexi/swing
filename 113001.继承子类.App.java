b220d658f7389d6c654f27e58f7c97275d98b9b5
fworeiaixuexi <74755055+fworeiaixuexi@users.noreply.github.com>
2020-11-30 15:06:03 +0800

    113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,7 @@
+public class App extends R {
+	public static void main(String[] args) {
+	System.out.println(boy_filePath);
+	System.out.println(girl_filePath);
+	System.out.println(array2d);
+	}
+}
