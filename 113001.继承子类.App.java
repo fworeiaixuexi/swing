5aec6a7002b29a50b2e5aa79082d0427511af554
fworeiaixuexi <74755055+fworeiaixuexi@users.noreply.github.com>
2020-11-30 15:37:45 +0800

    113001.继承子类.App.java

--- 113001.继承子类.App.java
+++ 113001.继承子类.App.java
@@ -1,7 +1,11 @@
 public class App extends R {
 	public static void main(String[] args) {
-	System.out.println(boy_filePath);
-	System.out.println(girl_filePath);
-	System.out.println(array2d);
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.println(R.array2d[row][column]);
+			}
+		}
 	}
 }
