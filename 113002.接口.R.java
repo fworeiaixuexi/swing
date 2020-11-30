1802e27d370bbeb63b8aab6b05a8c57012f7b1b7
fworeiaixuexi <74755055+fworeiaixuexi@users.noreply.github.com>
2020-11-30 15:28:10 +0800

    113002.接口.R.java

--- 113002.接口.R.java
+++ 113002.接口.R.java
@@ -1,5 +1,8 @@
 public interface R {
 	 String boy_filePath="img/boy.gif";
 	 String girl_filePath="ima/girl.gif";
-	 int [][]array2d={{1,2,3},{4,5,6}};
+	 int [][]array2d={
+			 {1,2,3},
+			 {4,5,6}
+			 };
 }
