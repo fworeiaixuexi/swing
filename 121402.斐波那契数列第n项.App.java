6cbbc2c1a3e938a59a16744f5e261d993ac9f1ab
fworeiaixuexi <74755055+fworeiaixuexi@users.noreply.github.com>
2020-12-14 15:41:38 +0800

    121402.斐波那契数列第n项.App.java

---
+++ 121402.斐波那契数列第n项.App.java
@@ -0,0 +1,26 @@
+package pac;
+
+import java.util.Scanner;
+
+public class App2 {
+	public static long fibonacci(long number) {
+        if ((number == 0) || (number == 1))
+            return number;
+        else
+            return fibonacci(number - 1) + fibonacci(number - 2);
+        }
+        public static void main(String[] args) {
+        	while (true) {
+        		Scanner scanner=new Scanner(System.in);
+            	int n=scanner.nextInt();
+            	if (0<n && n<=40) {
+            		System.out.printf("Fibonacci of %d is: %d\n",
+            	            n, fibonacci(n));
+    			}else {
+    				System.out.println("不在范围内，请输入正确的数值");
+    			}
+			}
+
+    }
+
+}
